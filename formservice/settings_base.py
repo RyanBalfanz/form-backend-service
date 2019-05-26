f95ea542d38b776be6e9f1cf9710b76720781d4a
Ryan Balfanz <rbalfanz@gmail.com>
2019-05-26 01:57:10 -0700

    Set STATIC_ROOT

--- formservice/settings_base.py
+++ formservice/settings_base.py
@@ -38,6 +38,11 @@ DATABASES = {
     'default': env.db(),
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 EMAIL_CONFIG = env.email_url('EMAIL_URL')
 
 vars().update(EMAIL_CONFIG)
