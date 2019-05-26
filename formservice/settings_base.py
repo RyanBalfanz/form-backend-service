111350123a298c57c2b46c441417b0f4dd0bdba5
Ryan Balfanz <rbalfanz@gmail.com>
2019-05-26 01:48:25 -0700

    Install WhiteNoise middleware

--- formservice/settings_base.py
+++ formservice/settings_base.py
@@ -11,11 +11,25 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
+# Application definition
+
 PROJECT_APPS = []
 
 INSTALLED_APPS += [
     'django_extensions',
 ] + PROJECT_APPS
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    # http://whitenoise.evans.io/en/stable/index.html#quickstart-for-django-apps
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
